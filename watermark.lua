385a326253be6285d6d3df752afb668c3acd102f
EldmonstretPasta <89604461+EldmonstretPasta@users.noreply.github.com>
2023-03-16 21:41:39 +0100

    Create watermark.lua

---
+++ watermark.lua
@@ -0,0 +1,25 @@
+local WmMain = Instance.new("ScreenGui") -- Creates the screengui which holds the text, ik i could've used drawing lib but this is easier lol
+local TextLabel = Instance.new("TextLabel")
+
+WmMain.Parent = game.CoreGui -- Sets parent to coregui so it's harder for games to detect
+WmMain.Name = "Watermark"
+
+local alreadyexists = game.CoreGui:FindFirstChild("ScreenGui")
+if alreadyexists then
+	alreadyexists:Destroy()
+end
+
+TextLabel.Parent = WmMain
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.Code
+TextLabel.TextColor3 = getgenv().wmcolor
+TextLabel.Text = getgenv().wmtext
+TextLabel.TextSize = 20.000
+
+game:GetService("RunService").RenderStepped:Connect(function()
+	if getgenv().displayname == true then
+		TextLabel.Text = getgenv().wmtext.. " | ".. game.Players.LocalPlayer.Name
+	end
+end)
